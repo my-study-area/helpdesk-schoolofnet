dbaef7c30fe3cdd655404a82956a3f268489c312
Adriano Avelino <adriano-st777@hotmail.com>
2020-09-25 22:26:00 -0300

    renomeia pacote

--- src/main/java/com/schoolofnet/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/schoolofnet/helpdesk/HelpdeskApplication.java
@@ -1,4 +1,4 @@
-package com.schoolofnet.Helpdesk;
+package com.schoolofnet.helpdesk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
