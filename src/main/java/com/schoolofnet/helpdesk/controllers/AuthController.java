73340b4429674c8f1d4be366f8d62e3f4370af66
Adriano Avelino <adriano-st777@hotmail.com>
2020-10-08 22:26:35 -0300

    adiciona mensagem de erro e link de registration no login

--- src/main/java/com/schoolofnet/helpdesk/controllers/AuthController.java
+++ src/main/java/com/schoolofnet/helpdesk/controllers/AuthController.java
@@ -4,11 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.schoolofnet.helpdesk.models.User;
+
 @Controller
 public class AuthController {
 	
 	@GetMapping("/login")
 	public String login(Model model) {
 		return "auth/login";
+	}
+	
+	@GetMapping("/registration")
+	public String registration(Model model) {
+		model.addAttribute("user", new User());
+		return "auth/registration";
 	}
 }
