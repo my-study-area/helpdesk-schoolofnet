d190e984204f109588d6af82319000aa0ea7e309
Adriano Avelino <adriano-st777@hotmail.com>
2020-10-16 17:27:59 -0300

    adiciona página de acesso negado

--- src/main/java/com/schoolofnet/helpdesk/controllers/HomeController.java
+++ src/main/java/com/schoolofnet/helpdesk/controllers/HomeController.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/")
 public class HomeController {
 	
 	@GetMapping
 	public String index(Model model) {
 		return "home/index";
+	}
+	
+	@GetMapping("/denied")
+	public String denied() {
+		return "home/403";
 	}
 }
