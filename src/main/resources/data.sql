c3142872a7d7a85ec896cc4a65fea9ed1640240d
Adriano Avelino <adriano-st777@hotmail.com>
2020-10-19 20:49:14 -0300

    adiciona interactions na carga inicial do banco de dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO users_roles (user_id, role_id) VALUES (4,1);
 insert into tickets (closed, created, description, finished, name, technician_id, user_id) values (null, '2020-10-13 19:56:28', 'decricao ticket 1', 0, 'nome do ticket', 3, 1 );
 insert into tickets (closed, created, description, finished, name, technician_id, user_id) values (null, '2020-10-14 09:10:00', 'Uma descrição do ticket 2', 0, 'ticket 2', 4, 1 );
 insert into tickets (closed, created, description, finished, name, technician_id, user_id) values (null, '2020-10-12 18:05:12', 'Uma descrição do ticket 3', 0, 'ticket 3', 1, 3 );
+
+insert into interactions (date_created, text, ticket_id, user_id) values ('2020-10-18 19:10:29', "teste", 1, 1);
+insert into interactions (date_created, text, ticket_id, user_id) values ('2020-10-18 19:14:12', "teste user 2", 1, 2);
